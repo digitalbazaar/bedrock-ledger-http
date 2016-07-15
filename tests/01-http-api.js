9ef3e2d6d76700c9f8323d1e40c9e3e4389a1765
Manu Sporny <msporny@digitalbazaar.com>
2016-07-15 15:49:15 -0400

    Add basic ledger configuration test.

--- tests/01-http-api.js
+++ tests/01-http-api.js
@@ -12,6 +12,13 @@ var bedrock = require('bedrock');
 var config = bedrock.config;
 var helpers = require('./helpers');
 var mockData = require('./mock.data');
+var request = require('request');
+request = request.defaults({json: true});
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
+// endpoints used by the tests
+var ledgerEndpoint = config.ledger.basePath;
 
 describe('DHS 2016 Ledger HTTP API', function() {
   before(function(done) {
@@ -22,7 +29,37 @@ describe('DHS 2016 Ledger HTTP API', function() {
   });
   describe('configuration', function() {
     it('should allow initial configuration', function(done) {
-      done();
+      var ledgerConfigurationEvent = {
+        '@context': 'https://w3id.org/flex/v1',
+        id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3',
+        name: 'dhs2016poc',
+        description: 'A proof of concept for a Verifiable Claims ledger',
+        type: 'LedgerConfigurationEvent',
+        storageMechanism: 'SequentialList',
+        consensusAlgorithm: {
+          type: 'ProofOfSignature2016',
+          approvedSigner: 'https://webville.va.us.gov/i/planning-department',
+          minimumSignaturesRequired: 1
+        },
+        previousEvent: 'urn:sha256:0000000000000000000000000000000000000000000000000000000000000000',
+        signature: {
+          type: 'LinkedDataSignature2016',
+          created: '2016-02-21T02:10:21Z',
+          creator: 'https://webville.va.us.gov/i/planning-department/keys/1',
+          signatureValue: 'cNJGLFqT/d/90D4GFzv...yKPiw=='
+        }
+      };
+
+      request.post({
+        url: ledgerEndpoint,
+        body: ledgerConfigurationEvent,
+        json: true
+      }, function(err, res, body) {
+        should.not.exist(err);
+        res.statusCode.should.equal(201);
+        should.exist(body);
+        done();
+      });
     });
     it('should not allow unsigned configuration', function(done) {
       done();
