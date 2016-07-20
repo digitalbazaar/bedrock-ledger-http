fe0f9713bfa69dc5330006038f30eebd8390a422
Manu Sporny <msporny@digitalbazaar.com>
2016-07-19 21:16:04 -0400

    Add ledger storage event and signature.

--- tests/01-http-api.js
+++ tests/01-http-api.js
@@ -24,6 +24,7 @@ jsigs.use('jsonld', bedrock.jsonld);
 
 // endpoints used by the tests
 var ledgerEndpoint = config.server.baseUri + config.ledger.basePath;
+var dhsLedgerEndpoint = ledgerEndpoint + '/dhs2016poc';
 
 // authorized signer URL
 var authorizedSignerUrl = config.server.baseUri + '/i/fema/keys/1';
@@ -34,7 +35,7 @@ var unauthorizedSignerUrl = config.server.baseUri + '/i/isis/keys/1';
 // base ledger configuration event
 var ledgerConfigurationEvent = {
   '@context': 'https://w3id.org/flex/v1',
-  id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3/events/genesis',
+  id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3/events/1',
   type: 'LedgerConfigurationEvent',
   ledgerConfig: {
     id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3',
@@ -48,7 +49,34 @@ var ledgerConfigurationEvent = {
       minimumSignaturesRequired: 1
     },
   },
-  previousEvent: 'urn:sha256:0000000000000000000000000000000000000000000000000000000000000000'
+  previousEvent: {
+    hash: 'urn:sha256:0000000000000000000000000000000000000000000000000000000000000000'
+  }
+};
+
+// ledger storage event - verifiable claim
+var ledgerStorageEvent = {
+  '@context': [
+    'https://w3id.org/flex/v1'/*,
+    'https://w3id.org/dhs/credentials/v1',*/
+  ],
+  id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3/events/2',
+  type: 'LedgerStorageEvent',
+  replacesObject: [{
+    id: 'https://example.us.gov/credentials/234234542',
+    type: ['Credential', 'EmergencyResponseCredential'],
+    claim: {
+      id: 'did:370d4e2c-8839-4588-9ff7-2fda89da341f',
+      emsLicense: {
+        id: 'ems:FF-37-48573',
+        status: 'valid'
+      }
+    }
+  }],
+  previousEvent: {
+    id: 'did:c02915fc-672d-4568-8e6e-b12a0b35cbb3/events/1',
+    hash: 'urn:sha256:'
+  }
 };
 
 describe('DHS 2016 Ledger HTTP API', function() {
@@ -69,7 +97,6 @@ describe('DHS 2016 Ledger HTTP API', function() {
         if(err) {
           return done(err);
         }
-        console.log(signedConfigEvent);
         request.post({
           url: ledgerEndpoint,
           body: signedConfigEvent,
@@ -93,7 +120,25 @@ describe('DHS 2016 Ledger HTTP API', function() {
   });
   describe('ledger writing', function() {
     it('should allow signed write', function(done) {
-      done();
+      done(); // FIXME: temporary
+      jsigs.sign(ledgerStorageEvent, {
+        algorithm: 'LinkedDataSignature2015',
+        privateKeyPem: mockData.agencies.fema.privateKey,
+        creator: authorizedSignerUrl
+      }, function(err, signedStorageEvent) {
+        if(err) {
+          return done(err);
+        }
+        request.post({
+          url: dhsLedgerEndpoint,
+          body: signedStorageEvent,
+          json: true
+        }, function(err, res, body) {
+          should.not.exist(err);
+          res.statusCode.should.equal(201);
+          done();
+        });
+      });
     });
     it('should allow signed update', function(done) {
       done();
