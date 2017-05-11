fc7c8a6e86aa6f0502a440b6233c5f45f820cc22
Matthew Collier <collier.matthew@gmail.com>
2017-05-11 15:25:33 -0400

    Update deps.

--- test/test.js
+++ test/test.js
@@ -4,15 +4,9 @@
 'use strict';
 
 const bedrock = require('bedrock');
-const brServer = require('bedrock-server');
 
 require('bedrock-express');
-require('bedrock-requirejs');
-require('bedrock-validation');
-require('bedrock-views');
-require('bedrock-identity');
 require('bedrock-identity-http');
-require('bedrock-key');
 require('bedrock-key-http');
 require('bedrock-ledger-http');
 
